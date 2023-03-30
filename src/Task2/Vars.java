5fdabca9f05660e11631a5c91e02ce589b834f8e
Kosyachenko Olga <kosya4enko.o@yandex.ru>
2023-03-30 05:17:59 +0300

    Промежуточная контрольная №2

---
+++ src/Task2/Vars.java
@@ -0,0 +1,10 @@
+package Task2;
+
+
+import java.util.ArrayList;
+
+public class Vars {
+    public static ArrayList<Toy> allToys = new ArrayList<Toy>();
+    public static ArrayList<Toy> winsToys = new ArrayList<Toy>();
+    public static String myFileName = "winnerTxt.txt";
+}
